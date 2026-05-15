e142bd428f770e995de3fa3d092e2989f60c757f
MuhammadAshiqAmeer <muhammadashiqameer@gmail.com>
2026-05-15 16:28:44 +0530

    init

--- src/config/settings.py
+++ src/config/settings.py
@@ -100,26 +100,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'mssql',
-#         'NAME': os.environ.get('DB_NAME'),        
-#         'USER': os.environ.get('DB_USER'),       
-#         'PASSWORD': os.environ.get('DB_PASSWORD'),
-#         'HOST': os.environ.get('DB_HOST'),        
-#         'PORT': os.environ.get('DB_PORT', 1433),  
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 18 for SQL Server',
-#             'extra_params': 'TrustServerCertificate=yes;Encrypt=no',
-#         },
-#     }
-# }
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME': os.environ.get('DB_NAME'),        
+        'USER': os.environ.get('DB_USER'),       
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),        
+        'PORT': os.environ.get('DB_PORT', 1433),  
+        'OPTIONS': {
+            'driver': 'ODBC Driver 18 for SQL Server',
+            'extra_params': 'TrustServerCertificate=yes;Encrypt=no',
+        },
     }
+}
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 # User Config
 AUTH_USER_MODEL = 'users.CustomUser'
 
