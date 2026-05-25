4559b4057c8ff9acaa658e8fcf3aefa24e5081f3
ashiqnexteons <ashiq.nexteons@gmail.com>
2026-05-25 13:53:29 +0530

    live check removed from leaves

--- src/handler.py
+++ src/handler.py
@@ -1,3 +1,5 @@
+import json
+import logging
 import os
 import pyodbc
 
@@ -7,6 +9,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from mangum import Mangum
 from config.asgi import application
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 TEXT_MIME_TYPES = [
     "application/json",
@@ -22,8 +27,12 @@ TEXT_MIME_TYPES = [
     "text/xml",
 ]
 
-lambda_handler = Mangum(
+asgi_handler = Mangum(
     application,
     lifespan="off",
     text_mime_types=TEXT_MIME_TYPES,
 )
+
+def lambda_handler(event, context):
+    # logger.info("EVENT: %s", json.dumps(event))
+    return asgi_handler(event, context)
